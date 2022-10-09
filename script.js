2c5b75425a91f163d206dbfe3906ca5ba5c4b592
nikosajadi <na.sajadii.29@gmail.com>
2022-10-09 02:17:39 +0200

    Pause or play Event Listener

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-const music = document.querySelector('Audio')
-const prevBtn = document.getElementById('prev')
-const playBtn = document.getElementById('prev')
-const nextBtn = document.getElementById('prev')
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
 
 
 //check if playing at the first time
 
 let isPlaying = false;
 
-//play
-function playsong() {
+// play
+function playSong() {
     isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
     music.play();
-}
+  }
 
 //pause
+
 function pauseSong() {
     isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
     music.pause();
-}
+  }
 
+// Pause or play Event Listener
+
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
